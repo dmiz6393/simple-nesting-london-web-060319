7d903a2adbaba3f633982b62fe513c0d10f858d7
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-08 09:03:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages_toS]
+     programmer_hash[:dennis_ritchie][:languages_toString]
 end
 
 def adding_matz
